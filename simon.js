071d38a951dd42fadd3c8b7f42462b29d4999288
jriggall <jriggall@hotmail.com>
2020-10-06 08:32:34 -0500

    Basic highlight function working

--- simon.js
+++ simon.js
@@ -1,11 +1,67 @@
+const topLeft = document.getElementById('topLeft')
+const topRight = document.getElementById('topRight')
+const botLeft = document.getElementById('botLeft')
+const botRight = document.getElementById('botRight')
+
+colorSeq = [topLeft, topRight, botRight, botLeft]
+
+let highlight = quadrant => {
+    return new Promise((resolve, reject) => {
+        if (quadrant == topLeft) {
+            quadrant.className = quadrant.className += ' TL-highlight'
+            setTimeout(() => {
+                quadrant.className = quadrant.className.replace(' TL-highlight', '')
+                resolve()
+            }, 1000);   
+        } else if (quadrant == topRight) {
+            quadrant.className = quadrant.className += ' TR-highlight'
+            setTimeout(() => {
+                quadrant.className = quadrant.className.replace(' TR-highlight', '')
+                resolve()
+            }, 1000);   
+        } else if (quadrant == botLeft) {
+            quadrant.className = quadrant.className += ' BL-highlight'
+            setTimeout(() => {
+                quadrant.className = quadrant.className.replace(' BL-highlight', '')
+                resolve()
+            }, 1000);   
+        } else if (quadrant == botRight) {
+            quadrant.className = quadrant.className += ' BR-highlight'
+            setTimeout(() => {
+                quadrant.className = quadrant.className.replace(' BR-highlight', '')
+                resolve()
+            }, 1000);   
+        } else {
+            reject('error in highlight')
+        }
+    })
+};
+
 const startGameBtn = document.getElementById('startGameBtn')
 
-startGameBtn.onclick = function() {
-    document.getElementById('win-msg-id').className += " show";
+startGameBtn.onclick = () => {
+    document.getElementById('win-msg-id').className += " show"
 };
 
 const exitBtn = document.getElementById('simonExitBtn')
 
-exitBtn.onclick = function() {
-    document.getElementById('gameover-msg-id').className += " show";
+exitBtn.onclick = () => {
+    document.getElementById('gameover-msg-id').className += " show"
 };
+
+const main = async () => {
+    try {
+        for (quadrant of colorSeq) {
+            await highlight(quadrant)
+        }
+    } catch (err) {
+        console.log('Error: ' + err)
+    }
+};
+
+main();
+/*
+let generateSequence = () => {
+    let seqLength = Math.random() *
+}
+*/
