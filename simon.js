198c9580a9aa41954ae63a7c586c0614f7f06cc0
jriggall <jriggall@hotmail.com>
2020-10-09 06:14:24 -0500

    Basic game functionality is working with proper win/game over screens

--- simon.js
+++ simon.js
@@ -10,20 +10,25 @@ const main = async () => {
     try {
         console.log('no board')
         game = new simonBoard()
-        console.log('board created')
-        game.addToSequence()
-        console.log('added to sequence')
-        console.log('sequence length = ' + game.currentSequence.length)
-        for (quadrant of game.currentSequence) {
-            await game.highlightQuadrant(quadrant)
-        }
-        console.log('highlighted')
-        game.canClick = true
+        game.iterateLevel()
+        console.log('level was iterated')
 
-        topRight.onclick = () => {game.quadrantClicked(topRight)}
-        topLeft.onclick = () => {game.quadrantClicked(topLeft)}
-        botRight.onclick = () => {game.quadrantClicked(botRight)}
-        botLeft.onclick = () => {game.quadrantClicked(botLeft)}
+        topRight.onclick = async () => {
+            await game.quadrantClicked(topRight)
+            game.iterateLevel()
+        }
+        topLeft.onclick = async () => {
+            await game.quadrantClicked(topLeft)
+            game.iterateLevel()
+        }
+        botRight.onclick = async () => {
+            await game.quadrantClicked(botRight)
+            game.iterateLevel()
+        }
+        botLeft.onclick = async () => {
+            await game.quadrantClicked(botLeft)
+            game.iterateLevel()
+        }
 
     } catch (err) {
         console.log('Error: ' + err)
@@ -35,22 +40,37 @@ const startGameBtn = document.getElementById('startGameBtn')
 
 startGameBtn.onclick = () => {
     main()
-    // document.getElementById('win-msg-id').className += " show"
 }
 
 // Exit Game Button
 const exitBtn = document.getElementById('simonExitBtn')
 
 exitBtn.onclick = () => {
-    document.getElementById('gameover-msg-id').className += ' show'
+    
 }
 
 class simonBoard {
     constructor() {
-        this.seqLength = 5
+        this.seqLength = 0
         this.currentSequence = []
         this.userSequenceGuess = []
         this.canClick = false
+    }
+
+    async iterateLevel() {
+        try {
+            this.userSequenceGuess = []
+            this.seqLength++
+            document.getElementById("levelHeader").innerHTML = "Level " + this.seqLength
+            console.log('seq length in itlvl = ' + this.seqLength)
+            this.addToSequence()
+            for (const quadrant of this.currentSequence) {
+                await this.highlightQuadrant(quadrant)
+            }
+            this.canClick = true
+        } catch(err) {
+            console.log('Error in iterateLevel function = ' + err)
+        }
     }
 
     addToSequence() {
@@ -77,57 +97,83 @@ class simonBoard {
     }
 
     // highlights quadrants from the list colorSeq
-     highlightQuadrant() {
+     highlightQuadrant(quadrant) {
         return new Promise((resolve, reject) => {
             try {
-            if (quadrant === topLeft) {
-                console.log('TL!!!')
-                quadrant.className = quadrant.className += ' TL-highlight'
-                setTimeout(() => {
-                    quadrant.className = quadrant.className.replace(' TL-highlight', '')
-                    setTimeout(() => {resolve()}, timeBetweenHighlights)
-                }, highlightTime);   
-            } else if (quadrant === topRight) {
-                console.log('TR!!!')
-                quadrant.className = quadrant.className += ' TR-highlight'
-                setTimeout(() => {
-                    quadrant.className = quadrant.className.replace(' TR-highlight', '')
-                    setTimeout(() => {resolve()}, timeBetweenHighlights)
-                }, highlightTime);  
-            } else if (quadrant === botLeft) {
-                console.log('BL!!!')
-                quadrant.className = quadrant.className += ' BL-highlight'
-                setTimeout(() => {
-                    quadrant.className = quadrant.className.replace(' BL-highlight', '')
-                    setTimeout(() => {resolve()}, timeBetweenHighlights)
-                }, highlightTime);  
-            } else if (quadrant === botRight) {
-                console.log('BR!!!')
-                quadrant.className = quadrant.className += ' BR-highlight'
-                setTimeout(() => {
-                    quadrant.className = quadrant.className.replace(' BR-highlight', '')
-                    setTimeout(() => {resolve()}, timeBetweenHighlights)
-                }, highlightTime);  
-            } else {
-                reject('error in highlightQuadrant function')
+                if (quadrant === topLeft) {
+                    console.log('TL!!!')
+                    quadrant.className = quadrant.className += ' TL-highlight'
+                    setTimeout(() => {
+                        quadrant.className = quadrant.className.replace(' TL-highlight', '')
+                        setTimeout(() => {resolve()}, timeBetweenHighlights)
+                    }, highlightTime);   
+                } else if (quadrant === topRight) {
+                    console.log('TR!!!')
+                    quadrant.className = quadrant.className += ' TR-highlight'
+                    setTimeout(() => {
+                        quadrant.className = quadrant.className.replace(' TR-highlight', '')
+                        setTimeout(() => {resolve()}, timeBetweenHighlights)
+                    }, highlightTime);  
+                } else if (quadrant === botLeft) {
+                    console.log('BL!!!')
+                    quadrant.className = quadrant.className += ' BL-highlight'
+                    setTimeout(() => {
+                        quadrant.className = quadrant.className.replace(' BL-highlight', '')
+                        setTimeout(() => {resolve()}, timeBetweenHighlights)
+                    }, highlightTime);  
+                } else if (quadrant === botRight) {
+                    console.log('BR!!!')
+                    quadrant.className = quadrant.className += ' BR-highlight'
+                    setTimeout(() => {
+                        quadrant.className = quadrant.className.replace(' BR-highlight', '')
+                        setTimeout(() => {resolve()}, timeBetweenHighlights)
+                    }, highlightTime);  
+                } else {
+                    reject('highlightQuadrant function rejected')
+                }
+            } catch (err) {
+                console.log('Error in highlightQuadrant: ' + err)
             }
-        } catch (err) {
-            console.log('Error in highlight says: ' + err)
-        }
         })
     }
 
     quadrantClicked(quadrant) {
-        if (!this.canClick) {
-            return
-        } else {
-            console.log(quadrant)
-        }
+        return new Promise((resolve, reject) => {
+            try {
+                if (!this.canClick) {
+                    resolve()
+                } else {
+                    console.log(quadrant)
+                    this.userSequenceGuess.push(quadrant)
+                    console.log('user sequence length is ' + this.userSequenceGuess.length)
+                    let curIndex = this.userSequenceGuess.length - 1
+                    if (this.userSequenceGuess[curIndex] === this.currentSequence[curIndex]) {
+                        if(this.userSequenceGuess.length === this.seqLength) {
+                            console.log('YOU WIN!!!!!!!')
+                            document.getElementById('win-msg-id').className += " show"
+                            document.getElementById('nextLevelBtn').onclick = () => {
+                                document.getElementById('win-msg-id').classList.remove("show")
+                                console.log('YOU HIT THE NEXT LEVEL!!!!!!!!!')
+                                this.canClick = false
+                                this.iterateLevel()
+                                console.log('after iteration the seq length is ' + this.currentSequence.length)
+                            }
+                        }
+                    } else {
+                        console.log('YOU LOSE!!!!!')
+                        document.getElementById('gameover-msg-id').className += ' show'
+                    }
+                }
+            } catch (err) {
+                console.log('Error in quadrantClicked function: ' + err)
+                reject('Error in quadrantClicked function promise')
+            }
+        })
     }
 
 
-}
 
+}
 
 // -------------------------------------------------------------------
 
@@ -297,4 +343,3 @@ class simonBoard {
 // exitBtn.onclick = () => {
 //     document.getElementById('gameover-msg-id').className += ' show'
 // }
-
