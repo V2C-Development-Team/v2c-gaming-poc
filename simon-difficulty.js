fe672f42f533126e7a6ce0c0ca463e95bd76dda4
jriggall <jriggall@hotmail.com>
2020-10-22 23:17:14 -0500

    Made the Simon difficulty menu buttons return to previous page once clicked for better flow

--- simon-difficulty.js
+++ simon-difficulty.js
@@ -2,14 +2,19 @@ const easyDiff = document.getElementById('easy-diff')
 const moderateDiff = document.getElementById('moderate-diff')
 const hardDiff = document.getElementById('hard-diff')
 
+let url = 'simonMenu.html'
+
 easyDiff.onclick = () => {
     localStorage.setItem('difficultyCookie', 'easy')
+    location.href = url
 }
 
 moderateDiff.onclick = () => {
     localStorage.setItem('difficultyCookie', 'moderate')
+    location.href = url
 }
 
 hardDiff.onclick = () => {
     localStorage.setItem('difficultyCookie', 'hard')
+    location.href = url
 }
